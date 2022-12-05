d20d6eb4c0ebe433109a34ea544ab3280b5cc955
knt0613pow <kimnamtae1230@gmail.com>
2022-12-05 23:48:30 +0900

    add model/layer
    add Spatial Attention / vgg16

--- Sphere_DELF/layer.py
+++ Sphere_DELF/layer.py
@@ -7,25 +7,60 @@ from torch.nn import functional as F
 from torch.nn import Module
 import math
 
+class PHD_conv2d_size1(Module):
+    def __init__(self, 
+                in_dim:int,
+                out_dim:int,
+                activation = 'ReLU',
+                ) -> None:
+        super(PHD_conv2d_size1, self).__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.stride = 1
+        self.activation = getattr(torch.nn, activation)()
+        shape = np.asarray([out_dim, in_dim, 1, 1])
+        self.weight = Parameter(torch.empty(*shape),)
+        self.bias = Parameter(torch.empty(shape[0]),) 
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+        if fan_in != 0:
+            bound = 1 / math.sqrt(fan_in)
+            init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input):
+        '''
+        input : (N, C, H = 1, W = 20 * (4**division))
+    
+        '''
+        output = F.conv2d(input,self.weight, self.bias, self.stride)
+        output = self.activation(output)
+        return output    
+
 class PHD_conv2d(Module):
     def __init__(self, 
-               in_dim:int,
-               out_dim:int,
-               stride : int,
-               device = None,
-               dtype = None,
-               activation = 'ReLU',
-               ) -> None:
+                in_dim:int,
+                out_dim:int,
+                stride : int,
+                activation = 'ReLU',
+                conv_table = None,
+                ) -> None:
         super(PHD_conv2d, self).__init__()
 
-        factory_kwargs = {'device': device, 'dtype': dtype}
+        assert conv_table is not None
+
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.stride = stride
         self.activation = getattr(torch.nn, activation)()
+        self.conv_table = conv_table  # (W, 10)
         shape = np.asarray([out_dim, in_dim, 10, 1])
-        self.weight = Parameter(torch.empty(*shape),)# **factory_kwargs)
-        self.bias = Parameter(torch.empty(shape[0]),) #**factory_kwargs)
+        self.weight = Parameter(torch.empty(*shape),)
+        self.bias = Parameter(torch.empty(shape[0]),) 
 
         self.reset_parameters()
 
@@ -37,8 +72,11 @@ class PHD_conv2d(Module):
             bound = 1 / math.sqrt(fan_in)
             init.uniform_(self.bias, -bound, bound)
 
-    def forward(self, input, conv_table):
-        x = input[..., conv_table].squeeze()
+    def forward(self, input):
+        '''
+        input : (N, C, H = 1, W = 20 * (4**division))
+        '''
+        x = input[..., self.conv_table.T].squeeze()
         output = F.conv2d(x,self.weight, self.bias, self.stride)
         output = self.activation(output)
         return output
@@ -50,7 +88,7 @@ def PHD_maxpool(input, adj_table, pooling_table):
     pooling_table (20*(4**(division-1)))
 '''
     
-    output = input[..., adj_table].squeeze() # (N, C, 4, W,)
+    output = input[..., adj_table.T].squeeze() # (N, C, 4, W,)
     output = output[..., pooling_table] # (N, C, 4, W/4, )
     output = torch.max(output, dim=2,keepdim=True)[0] # (N, C/ 1, W/4)
     return output
@@ -62,78 +100,26 @@ def PHD_avgpool(input):
     output = torch.mean(input, (2, 3)) # (N, C)
     return output    
 
+class PHD_SpatialAttention(Module):
+    def __init__(self, in_c):
+        super(PHD_SpatialAttention, self).__init__()
+        self.conv1  = PHD_conv2d_size1(in_c, 512)
+        self.conv2  = PHD_conv2d_size1(512, 1, activation = 'Softplus')
 
-'''
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-import numpy as np
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.conv2(output)
 
-def conv2d(input, in_dim, out_dim, conv_table, name, 
-           reuse=False, stride=1, activation='elu', padding='VALID'):
-
-    shape = np.asarray([10, 1, in_dim, out_dim])
-    with tf.variable_scope(name, reuse= reuse):
-        weight = tf.get_variable('weight', 
-                     shape= shape, 
-                     initializer = tf.truncated_normal_initializer(stddev=0.1))
-        bias = tf.get_variable('bias', 
-                     shape= shape[-1], 
-                     initializer= tf.constant_initializer(0))
-        # width = 20 * 4**subdivision
-        # input = (n_batch, 1, width, n_channel), conv_table (kernelsize, width)
-        # Turns x into an array of shape (n_batch, 1, kernel=10, width, n_channel)
-        x = tf.gather(input, conv_table, axis=2)
-        # squeeze the array into shape (n_batch, kernel=10, width, n_channel)
-        x = tf.squeeze(x, axis=1)
-        output = tf.nn.conv2d(x, weight, [1, stride, stride, 1], padding) + bias
-        if activation == 'elu':
-            output = tf.nn.elu(output)
-        
         return output
 
-def channel_conv(input, in_dim, out_dim, name, reuse=False, 
-                 stride=1, activation='elu', padding='VALID'):
 
-    shape = np.asarray([1, 1, in_dim, out_dim])
-    with tf.variable_scope(name, reuse= reuse):
-        weight = tf.get_variable('weight', 
-                     shape= shape, 
-                     initializer = tf.truncated_normal_initializer(stddev=0.1))
-        bias = tf.get_variable('bias', 
-                     shape= shape[-1], 
-                     initializer= tf.constant_initializer(0))
-        # input = (n_batch, 1, width, n_channel)
-        output = tf.nn.conv2d(input, weight, [1, stride, stride, 1], padding) + bias
-
-        if activation == 'elu':
-            output = tf.nn.elu(output)
-        return output
-
-def maxpool(x, adj_table, pooling_table):
-
-    # input = (n_batch, 1, width, n_channel)
-    # Turns x into an array of shape (n_batch, 1, kernel=4, width, n_channel)
-    x = tf.gather(x, adj_table, axis=2)
-    # squeeze the array into shape (n_batch, kernel=4, width, n_channel)
-    x = tf.squeeze(x, axis=1)
-    # pool_table make the subdivision-1, with size: (width/4)
-    # Picks out correct pool indexes (n_batch, kernel=4, width/4, n_channel)
-    x = tf.gather(x, pooling_table, axis=2)
-    # Max from pool (n_batch, 1, width/4, n_channel)
-    x = tf.reduce_max(x, axis=1, keepdims=True)
-    return x
-
-def upsample(x, upsample_table):
-    # input = (n_batch, 1, width, n_channel)
-    # Turns x into an array of shape (n_batch, 1, width*4, n_channel)
-    x = tf.keras.layers.UpSampling2D((1, 4))(x)
-    # unpooling shape (n_batch, 1, width*4, n_channel)
-    x = tf.gather(x, upsample_table, axis=2)
-    return x
-
-def avgpool(x):
-    x = tf.reduce_mean(x, axis=[1, 2])
-    return x
-'''
+if __name__ == '__main__':
+    from maketable import *
+    cv_table = make_conv_table(3) #  (1280, 10)
+    # conv = PHD_conv2d(512, 512, 1, conv_table = cv_table)
+    input = Tensor(np.ones((25, 512, 1, 1280)))
+    # output = conv(input)
+    att = PHD_SpatialAttention(512)
+    output = att(input)
 
       
--- Sphere_DELF/model.py
+++ Sphere_DELF/model.py
@@ -26,46 +26,63 @@ class MNIST_net(nn.Module) :
         return out
 
 
-'''
-def MNIST_net(x, conv_tables, adj_tables, pooling_tables, div):
-    conv1 = PHD_conv2d(x, in_dim=1, out_dim=16, conv_table=conv_tables[div].T, name='conv_1')
-    pool1 = PHD_maxpool(conv1, adj_tables[div].T, pooling_tables[div-1].T)
 
-    conv2 = PHD_conv2d(pool1, in_dim=16, out_dim=32, conv_table=conv_tables[div-1].T, name='conv_2')
-    pool2 = PHD_maxpool(conv2, adj_tables[div-1].T, pooling_tables[div-2].T)
+class PHD_VGG16(nn.Module):
+    def __init__(self, conv_tables, adj_tables, pooling_tables, div):
+        super(PHD_VGG16, self).__init__()
+        self.conv_tables = conv_tables
+        self.adj_tables = adj_tables
+        self.pooling_tables = pooling_tables
+        self.div = div
 
-    conv3 = PHD_conv2d(pool2, in_dim=32, out_dim=10, conv_table=conv_tables[div-2].T, name='conv_3')
-    out = PHD_avgpool(conv3)
-    return out
+        self.layer1 = nn.Sequential(
+            PHD_conv2d(in_dim = 3, out_dim =64, stride = 1, conv_table = conv_tables[div]),
+            PHD_conv2d(in_dim = 64, out_dim = 64, stride = 1, conv_table = conv_tables[div]),
+        )
+        self.layer2 = nn.Sequential(
+            PHD_conv2d(in_dim = 64, out_dim =128, stride = 1, conv_table = conv_tables[div-1]),
+            PHD_conv2d(in_dim = 128, out_dim = 128, stride = 1, conv_table = conv_tables[div-1]),
+        )
+        self.layer3 = nn.Sequential(
+            PHD_conv2d(in_dim = 128, out_dim =256, stride = 1, conv_table = conv_tables[div-2]),
+            PHD_conv2d(in_dim = 256, out_dim = 256, stride = 1, conv_table = conv_tables[div-2]),
+            PHD_conv2d(in_dim = 256, out_dim = 256, stride = 1, conv_table = conv_tables[div-2]),
+        )
+
+        self.layer4 = nn.Sequential(
+            PHD_conv2d(in_dim = 256, out_dim =512, stride = 1, conv_table = conv_tables[div-3]),
+            PHD_conv2d(in_dim = 512, out_dim = 512, stride = 1, conv_table = conv_tables[div-3]),
+            PHD_conv2d(in_dim = 512, out_dim = 512, stride = 1, conv_table = conv_tables[div-3]),
+        )
+
+        self.layer5 = nn.Sequential(
+            PHD_conv2d(in_dim = 512, out_dim =512, stride = 1, conv_table = conv_tables[div-4]),
+            PHD_conv2d(in_dim = 512, out_dim = 512, stride = 1, conv_table = conv_tables[div-4]),
+            PHD_conv2d(in_dim = 512, out_dim = 512, stride = 1, conv_table = conv_tbales[div-4]),
+        )
+    def forward(self, x):
+        l1 = self.layer1(x)
+        #adj_table, pooling_table
+        l1 = PHD_maxpool(l1, adj_table[div], pooling_table[div])
+        l2 = self.layer2(l1)
+        l2 = PHD_maxpool(l2, adj_table[div-1], pooling_table[div-1])
+        l3= self.layer3(l2)
+        l3 = PHD_maxpool(l3, adj_table[div-2], pooling_table[div-2])
+        l4= self.layer3(l3)
+        l4 = PHD_maxpool(l4, adj_table[div-3], pooling_table[div-3])
+        l5= self.layer3(l4)
+        l5 = PHD_maxpool(l5, adj_table[div-4], pooling_table[div-4])
+        return l5
 
 
-def MNIST_net(x, conv_tables, adj_tables, pooling_tables, div):
 
-    conv1 = conv2d(x, in_dim=1, out_dim=16, conv_table=conv_tables[div].T, name='conv_1')
-    pool1 = maxpool(conv1, adj_tables[div].T, pooling_tables[div-1].T)
-
-    conv2 = conv2d(pool1, in_dim=16, out_dim=32, conv_table=conv_tables[div-1].T, name='conv_2')
-    pool2 = maxpool(conv2, adj_tables[div-1].T, pooling_tables[div-2].T)
-
-    conv3 = conv2d(pool2, in_dim=32, out_dim=10, conv_table=conv_tables[div-2].T, name='conv_3')
-    out = avgpool(conv3)
-
-    return out
-
-
-
-def auto_encoder(x, conv_tables, adj_tables, pooling_tables, upsample_tables, div):
-
-    conv1 = conv2d(x, in_dim=3, out_dim=128, conv_table=conv_tables[div].T, name='block1_conv1')
-    pool1 = maxpool(conv1, adj_tables[div].T, pooling_tables[div-1].T)
-    conv2 = conv2d(pool1, in_dim=128, out_dim=256, conv_table=conv_tables[div-1].T, name='block2_conv1')
-    pool2 = maxpool(conv2, adj_tables[div-1].T, pooling_tables[div-2].T)
-    conv3 = conv2d(pool2, in_dim=256, out_dim=512, conv_table=conv_tables[div-2].T, name='block3_conv1')
-
-    latent = channel_conv(conv3, in_dim=512, out_dim=256, name='channel_conv')
-    upsample1 = upsample(latent, upsample_tables[div-2].T)
-    de_conv2 = conv2d(upsample1, in_dim=256, out_dim=128, conv_table=conv_tables[div-1].T, name='block4_deconv1')
-    upsample2 = upsample(de_conv2, upsample_tables[div-1].T)
-    de_conv1 = conv2d(upsample2, in_dim=128, out_dim=14, conv_table=conv_tables[div].T, name='block5_deconv1')
-    return de_conv1
-'''
+if __name__ == '__main__':
+    subdivision = 5
+    conv_tables = []
+    adj_tables = []
+    for i in range(0, subdivision+1):
+        conv_tables.append(make_conv_table(i))
+        adj_tables.append(make_adjacency_table(i))
+        
+    pooling_tables = make_pooling_table(subdivision+1)
+    model = PHD_VGG16(conv_tables, adj_tables, pooling_tables, subdivision)
